7b71512633848eb18febd6693ce1c1cec1200cd2
Noah Rinehart <rinehart.noah@gmail.com>
2017-11-11 18:26:08 -0600

    added methods and classes

--- facialchoose.py
+++ facialchoose.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 """
 Created on Sat Nov 11 15:27:47 2017
 
@@ -7,43 +8,57 @@ Created on Sat Nov 11 15:27:47 2017
 
 ########### Python 3.2 #############
 import http.client, urllib.request, urllib.parse, urllib.error, base64, sys
+import cv2
+import json
+
+cap = cv2.VideoCapture(0)
+
+filepath = 'test.png'
 
 key = '44be13f4f58c4bdfb18a4d9ffd8716c1'
 
-content= input("url or file?:")
+class Emotions:
+    def __init__(self, emotions):
+        scores = emotions['scores']
+        self.anger = scores['anger']
+        self.contempt = scores['contempt']
+        self.disgust = scores['disgust']
+        self.fear = scores['fear']
+        self.happiness = scores['happiness']
+        self.neutral = scores['neutral']
+        self.sadness = scores['sadness']
+        self.surprise = scores['surprise']
 
-if content=='url':
-    urlname = input("enter url:")
+# Saves picture taken to filepath
+def fetchFrame():
+    ret, frame = cap.read()
+    ret, frame = cap.read()
+    ret, frame = cap.read()
+    cv2.imwrite(filepath, frame)
+
+# Sends request to microsoft emotions api and returns emotion values
+def fetchEmotions():
     headers = {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': key,
+        'Content-Type': 'application/octet-stream',
+        'Ocp-Apim-Subscription-Key': key,
     }
-    
-    body = "{ 'url': '"+ urlname +"' }"
 
-        
-        
-else:
-    filepath = input("path of file:")
-    
-    headers = {
-            'Content-Type': 'application/octet-stream',
-            'Ocp-Apim-Subscription-Key': key,
-    }
-    
-    body = open(filepath,'rb').read()
-
-params = urllib.parse.urlencode({
-    })
-    
-try:
-    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
-    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
-    response = conn.getresponse()
-    data = response.read()
-    print(data)
-    conn.close()
-except Exception as e:
-    print(e.args)
+    try:
+        body = open(filepath,'rb').read()
+        conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+        conn.request("POST", "/emotion/v1.0/recognize", body, headers)
+        response = conn.getresponse()
+        data = response.read()
+        conn.close()
+        return data
+    except Exception as e:
+        print(e.args)
+    return None
 
 ####################################
+
+if __name__ == "__main__":
+    fetchFrame()
+    emotions = fetchEmotions()
+    e = Emotions(json.loads(emotions)[0])
+    print(e.sadness)
